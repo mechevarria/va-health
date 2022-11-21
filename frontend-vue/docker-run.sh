fbd55fb284060ced19d1ef384448b1fd66cd4333
mechevar <michael.echevarria@symphonyai.com>
2022-11-21 17:00:02 -0500

    update for new project

--- frontend-vue/docker-run.sh
+++ frontend-vue/docker-run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+KEYCLOAK=false
+KEYCLOAK_URL=http://localhost
+
+docker rm frontend-vue
+
 docker run \
     -p 80:80 \
+    --name frontend-vue \
     --env KEYCLOAK=$KEYCLOAK \
     --env KEYCLOAK_URL=$KEYCLOAK_URL \
-    quay.io/mechevarria/demo-nginx-vue
+    quay.io/mechevarria/frontend-vue
