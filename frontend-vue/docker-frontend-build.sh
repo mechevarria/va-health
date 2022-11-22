ade4f279879e10c4c05a4cbc361200a1d75cf08a
mechevar <michael.echevarria@symphonyai.com>
2022-11-22 14:49:18 -0500

    working frontend dockerfile and config

--- frontend-vue/docker-frontend-build.sh
+++ frontend-vue/docker-frontend-build.sh
@@ -5,4 +5,4 @@ npm run build
 
 docker build -t quay.io/mechevarria/frontend-vue .
 
-docker push quay.io/mechevarria/frontend-vue
+# docker push quay.io/mechevarria/frontend-vue
--- frontend-vue/docker-frontend-run.sh
+++ frontend-vue/docker-frontend-run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+# not using authentication right now
 KEYCLOAK=false
 KEYCLOAK_URL=http://localhost
 
-docker rm frontend-vue
+# create network for other containers to communicate via name
+docker network create app-net
 
 docker run \
+    --rm \
+    -d \
     -p 80:80 \
     --name frontend-vue \
+    --network app-net \
     --env KEYCLOAK=$KEYCLOAK \
     --env KEYCLOAK_URL=$KEYCLOAK_URL \
     quay.io/mechevarria/frontend-vue
