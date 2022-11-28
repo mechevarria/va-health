42284667470c7d1bf217fa3360b77ac66a7d1597
ejm <ejm@ejms-mac.local>
2022-11-28 15:22:31 -0600

    Updated graph to return nodes and edges from platform

--- api-flask/resources/graph.py
+++ api-flask/resources/graph.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
@@ -7,10 +9,13 @@ from db import user
 
 blp = Blueprint("graph", __name__, description="Operations on graph")
 
+# @blp.route("/graph/<string:network_id>")
 @blp.route("/graph")
 class GraphService(MethodView):
+  # def get(self, network_id):
   def get(self):
-    data = [
+    """Returns nodes in this format for network graph
+        data = [
             {'from': 'A', 'to': 'C'},
             {'from': 'A', 'to': 'D'},
             {'from': 'A', 'to': 'E'},
@@ -18,6 +23,32 @@ class GraphService(MethodView):
             {'from': 'A', 'to': 'G'},
             {'from': 'B', 'to': 'C'},
             {'from': 'B', 'to': 'D'}
-        ]
+        ]    
+    """
+    src = user['connection'].get_source(name=user['source_name'])
+    #TODO: Remove hardwired and get id from user.  Once filter enabled
+    network_id = -4920673681958740057
+    nw = src.get_network(id=network_id)
+    node_groups = nw.get_node_groups()
+    print(len(node_groups))
+
+    groups = {}
+    for g in node_groups:
+        grp = src.get_group(id=g['id'])
+        g['rows'] = set(grp['row_indices'])
+        groups[g['id']] = g
+      
+    combs = list(combinations(groups.keys(), 2))
+
+    data = []
+
+    #create nodes with edges to self to ensure all nodes make it into the graph
+    for f, t in combs:
+      data.append({'from': f, 'to': f})
+
+    #Create Nodes with edges
+    for f, t in combs:
+      if groups[f]['rows'].isdisjoint(groups[t]['rows']): data.append({'from': f, 'to': t})
+
     return jsonify(data)
 
--- api-flask/resources/kpi.py
+++ api-flask/resources/kpi.py
@@ -13,16 +13,18 @@ class KPIService(MethodView):
   @blp.response(200, KpiCardSchema(many=True))
   def get(self):
     '''Gets all KPI values for the entire source'''
-    print('in kpi')
-    src = user['connection'].get_source(name=user['source_name'])
-    src.row_count()
+    try:
+      src = user['connection'].get_source(name=user['source_name'])
 
-    #Get number of rows
-    kpis.append({"name": "Total Patients", "value": src.row_count})
-    kpis.append({"name": "Vaccination Precentage", "value": -1})
-    kpis.append({"name": "Hospitilization Rate", "value": -1})
-    kpis.append({"name": "Outpatient Visits", "value": -1})
-    kpis.append({"name": "TBD", "value": -1})
+      kpis = []
+      #Get number of rows
+      kpis.append({"name": "Total Patients", "value": src.row_count})
+      kpis.append({"name": "Vaccination Precentage", "value": -1})
+      kpis.append({"name": "Hospitilization Rate", "value": -1})
+      kpis.append({"name": "Outpatient Visits", "value": -1})
+      kpis.append({"name": "TBD", "value": -1})
+    except:
+      abort(404, message="Error getting KPI from source")
 
     return kpis
 
