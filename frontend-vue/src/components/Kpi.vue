8405c927ba0ec55cc89e01fd7495973bae14bafe
mechevar <michael.echevarria@symphonyai.com>
2022-11-30 16:41:20 -0500

    call for backend data

--- frontend-vue/src/components/Kpi.vue
+++ frontend-vue/src/components/Kpi.vue
@@ -1,37 +1,83 @@
 <template>
-  <div class="card-deck">
-    <div class="card mb-4 mt-2 text-white bg-danger">
-      <div class="card-body">
-        Total Patients
-        <h4>123k</h4>
+  <span>
+    <h4>Clinical Dashboard <i class="spinner-border spinner-border-sm mb-1" v-if="isBusy"></i></h4>
+    <div class="card-deck">
+      <div class="card mb-4 mt-2 text-white bg-danger">
+        <div class="card-body">
+          <span v-if=data[0]>
+            {{ data[0].name }}
+            <h4>{{ data[0].value }}</h4>
+          </span>
+        </div>
+      </div>
+      <div class="card mb-4 mt-2 text-white bg-warning">
+        <div class="card-body">
+          <span v-if=data[1]>
+            {{ data[1].name }}
+            <h4>{{ data[1].value }}</h4>
+          </span>
+        </div>
+      </div>
+      <div class="card mb-4 mt-2 text-white bg-primary">
+        <div class="card-body">
+          <span v-if=data[2]>
+            {{ data[2].name }}
+            <h4>{{ data[2].value }}</h4>
+          </span>
+        </div>
+      </div>
+      <div class="card mb-4 mt-2 text-white bg-success">
+        <div class="card-body">
+          <span v-if=data[3]>
+            {{ data[3].name }}
+            <h4>{{ data[3].value }}</h4>
+          </span>
+        </div>
+      </div>
+      <div class="card mb-4 mt-2 text-white bg-info">
+        <div class="card-body">
+          <span v-if=data[4]>
+            {{ data[4].name }}
+            <h4>{{ data[4].value }}</h4>
+          </span>
+        </div>
       </div>
     </div>
-    <div class="card mb-4 mt-2 text-white bg-warning">
-      <div class="card-body">
-        Vaccination Percentage
-        <h4>123k</h4>
-      </div>
-    </div>
-    <div class="card mb-4 mt-2 text-white bg-primary">
-      <div class="card-body">
-        todo 3
-      </div>
-    </div>
-    <div class="card mb-4 mt-2 text-white bg-success">
-      <div class="card-body">
-        todo 4
-      </div>
-    </div>
-    <div class="card mb-4 mt-2 text-white bg-info">
-      <div class="card-body">
-        todo 5
-      </div>
-    </div>
-  </div>
+  </span>
 </template>
 
 <script>
+import axios from 'axios'
+import msgMixin from '../mixins/msg-mixin'
+
 export default {
-  name: 'AppKpi'
+  name: 'AppKpi',
+  mixins: [msgMixin],
+  data() {
+    return {
+      data: [],
+      isBusy: true
+    }
+  },
+  methods: {
+    getKpi() {
+      const url = '/api/kpi'
+      axios
+        .get(url)
+        .then((res) => {
+          this.data = res.data
+        })
+        .catch((err) => {
+          console.error(err)
+          this.errorMsg(err.message)
+        })
+        .finally(() => {
+          this.isBusy = false
+        })
+    }
+  },
+  created() {
+    this.getKpi()
+  }
 }
 </script>
