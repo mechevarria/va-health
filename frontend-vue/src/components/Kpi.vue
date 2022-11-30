e02285d5d5cbd47ba6feb0765eaeb705d879bb67
mechevar <michael.echevarria@symphonyai.com>
2022-11-30 15:15:15 -0500

    initial cards

---
+++ frontend-vue/src/components/Kpi.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-deck">
+    <div class="card mb-4 mt-2 text-white bg-danger">
+      <div class="card-body">
+        Total Patients
+        <h4>123k</h4>
+      </div>
+    </div>
+    <div class="card mb-4 mt-2 text-white bg-warning">
+      <div class="card-body">
+        Vaccination Percentage
+        <h4>123k</h4>
+      </div>
+    </div>
+    <div class="card mb-4 mt-2 text-white bg-primary">
+      <div class="card-body">
+        todo 3
+      </div>
+    </div>
+    <div class="card mb-4 mt-2 text-white bg-success">
+      <div class="card-body">
+        todo 4
+      </div>
+    </div>
+    <div class="card mb-4 mt-2 text-white bg-info">
+      <div class="card-body">
+        todo 5
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppKpi'
+}
+</script>
