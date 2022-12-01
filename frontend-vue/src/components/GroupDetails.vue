95a92ad87c43c1b82630e94f9baa4712fcbb8ce6
mechevar <michael.echevarria@symphonyai.com>
2022-12-01 12:47:55 -0500

    add stubbed details component

---
+++ frontend-vue/src/components/GroupDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <h5>Group {{groupName}} Demographics</h5>
+    <router-view></router-view>
+  </span>
+</template>
+
+<script>
+
+export default {
+  name: 'AppGroupDetail',
+  data() {
+    return {
+      groupName: 'S7'
+    }
+  }
+}
+</script>
