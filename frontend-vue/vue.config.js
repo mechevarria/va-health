0f3bd9803c748e86d8000c3264b21f9db7a43f04
mechevar <michael.echevarria@symphonyai.com>
2022-12-01 13:51:43 -0500

    use proper local address

--- frontend-vue/vue.config.js
+++ frontend-vue/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
                 }
             },
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'http://127.0.0.1:5000',
                 secure: false,
                 pathRewrite: {
                     '^/api': ''
