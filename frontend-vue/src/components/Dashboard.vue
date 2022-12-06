9296ed098bffae8fdeb37d8930424f1f51f202e5
mechevar <michael.echevarria@symphonyai.com>
2022-12-06 15:02:54 -0500

    add stubbed filter modal

--- frontend-vue/src/components/Dashboard.vue
+++ frontend-vue/src/components/Dashboard.vue
@@ -1,10 +1,15 @@
 <template>
   <span>
-    <h4 class="d-flex justify-content-between align-items-center">Clinical Dashboard <button class="btn btn-primary"><i class="cil-filter btn-icon mr-1"></i>Filter</button></h4>
+    <h4 class="d-flex justify-content-between align-items-center">Clinical Dashboard
+      <button class="btn btn-primary" v-b-modal.filter-modal><i class="cil-filter btn-icon mr-1"></i>Filter</button>
+    </h4>
     <AppKpi />
     <AppNetwork />
     <AppGroup />
     <AppGroupDetails />
+    <b-modal id="filter-modal" title="BootstrapVue">
+      <p class="my-4">Filter options will go here</p>
+    </b-modal>
     <router-view></router-view>
   </span>
 </template>
