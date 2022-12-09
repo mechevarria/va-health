ba6cd9912a9422c61231f9e330f0fa17b47b4bda
mechevar <michael.echevarria@symphonyai.com>
2022-12-09 10:38:27 -0500

    implement filter watching

--- frontend-vue/src/components/Kpi.vue
+++ frontend-vue/src/components/Kpi.vue
@@ -1,9 +1,9 @@
 <template>
   <span>
-    <i class="spinner-border spinner-border-sm mb-1 ml-1" v-if="isBusy"></i>
     <div class="card-deck">
       <div class="card mb-4 mt-2 text-white bg-danger">
         <div class="card-body">
+          <i class="spinner-border spinner-border-sm mb-1 ml-1 mt-1" v-if="isBusy"></i>
           <span v-if=data[0]>
             {{ data[0].name }}
             <h4>{{ data[0].value }}</h4>
@@ -12,6 +12,7 @@
       </div>
       <div class="card mb-4 mt-2 text-white bg-warning">
         <div class="card-body">
+          <i class="spinner-border spinner-border-sm mb-1 ml-1 mt-1" v-if="isBusy"></i>
           <span v-if=data[1]>
             {{ data[1].name }}
             <h4>{{ data[1].value }}</h4>
@@ -20,6 +21,7 @@
       </div>
       <div class="card mb-4 mt-2 text-white bg-primary">
         <div class="card-body">
+          <i class="spinner-border spinner-border-sm mb-1 ml-1 mt-1" v-if="isBusy"></i>
           <span v-if=data[2]>
             {{ data[2].name }}
             <h4>{{ data[2].value }}</h4>
@@ -28,6 +30,7 @@
       </div>
       <div class="card mb-4 mt-2 text-white bg-success">
         <div class="card-body">
+          <i class="spinner-border spinner-border-sm mb-1 ml-1 mt-1" v-if="isBusy"></i>
           <span v-if=data[3]>
             {{ data[3].name }}
             <h4>{{ data[3].value }}</h4>
@@ -36,6 +39,7 @@
       </div>
       <div class="card mb-4 mt-2 text-white bg-info">
         <div class="card-body">
+          <i class="spinner-border spinner-border-sm mb-1 ml-1 mt-1" v-if="isBusy"></i>
           <span v-if=data[4]>
             {{ data[4].name }}
             <h4>{{ data[4].value }}</h4>
@@ -47,11 +51,13 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import axios from 'axios'
 import msgMixin from '../mixins/msg-mixin'
 
 export default {
   name: 'AppKpi',
+  computed: mapState(['filterId']),
   mixins: [msgMixin],
   data() {
     return {
@@ -61,7 +67,12 @@ export default {
   },
   methods: {
     getKpi() {
-      const url = '/api/kpi'
+      this.isBusy = true
+      this.data = true
+      let url = '/api/kpi'
+      if (this.filterId > 0) {
+        url = url + `/${this.filterId}`
+      }
       axios
         .get(url)
         .then((res) => {
@@ -74,6 +85,11 @@ export default {
         .finally(() => {
           this.isBusy = false
         })
+    }
+  },
+  watch: {
+    filterId() {
+      this.getKpi()
     }
   },
   created() {
