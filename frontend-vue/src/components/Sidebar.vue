b6a7aab96e686bb82e3129d04dd056f9bb12f887
mechevar <michael.echevarria@symphonyai.com>
2022-12-13 14:27:00 -0500

    add compare component

--- frontend-vue/src/components/Sidebar.vue
+++ frontend-vue/src/components/Sidebar.vue
@@ -1,29 +1,21 @@
 <template>
-  <div
-    class="c-sidebar c-sidebar-dark c-sidebar-fixed"
-    :class="{ 'c-sidebar-minimized': isSidebarMin, 'c-sidebar-show': isSidebarShown }"
-    id="sidebar"
-  >
+  <div class="c-sidebar c-sidebar-dark c-sidebar-fixed"
+    :class="{ 'c-sidebar-minimized': isSidebarMin, 'c-sidebar-show': isSidebarShown }" id="sidebar">
     <div class="c-sidebar-brand d-md-down-none">
       <router-link to="/">
-        <img
-          class="c-sidebar-brand-full"
-          src="../assets/symphony-ai-logo-white.svg"
-          alt="SAI logo"
-          width="150"
-        />
-        <img
-          class="c-sidebar-brand-minimized"
-          src="../assets/symphony-ai-icon-white.svg"
-          width="30"
-          alt="SAI Logo"
-        />
+        <img class="c-sidebar-brand-full" src="../assets/symphony-ai-logo-white.svg" alt="SAI logo" width="150" />
+        <img class="c-sidebar-brand-minimized" src="../assets/symphony-ai-icon-white.svg" width="30" alt="SAI Logo" />
       </router-link>
     </div>
     <ul class="c-sidebar-nav" @mouseenter="mouseEnter" @mouseleave="mouseLeave">
       <li class="c-sidebar-nav-item">
         <router-link class="c-sidebar-nav-link" to="/home/dashboard" @click.native="closeOnMobile">
           <i class="c-sidebar-nav-icon cil-speedometer"></i> Dashboard
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link class="c-sidebar-nav-link" to="/home/compare" @click.native="closeOnMobile">
+          <i class="c-sidebar-nav-icon cil-blur-linear"></i> Group Compare
         </router-link>
       </li>
       <li class="c-sidebar-nav-item">
