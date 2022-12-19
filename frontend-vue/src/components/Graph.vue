2a9baeffb7684acea126b8343a6140921291b941
mechevar <michael.echevarria@symphonyai.com>
2022-12-19 11:05:22 -0500

    formatting and trying to update colors only

--- frontend-vue/src/components/Graph.vue
+++ frontend-vue/src/components/Graph.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <span>Patient Segments <i class="spinner-border spinner-border-sm mr-1" v-if="isBusy"></i></span>
+      <span
+        >Patient Segments
+        <i class="spinner-border spinner-border-sm mr-1" v-if="isBusy"></i
+      ></span>
 
-      <b-dropdown class="float-right" id="color-dropdown" right text="Right align" :disabled="isBusy">
+      <b-dropdown
+        class="float-right"
+        id="color-dropdown"
+        right
+        text="Right align"
+        :disabled="isBusy"
+      >
         <template #button-content>
-          <i class="cil-contrast mr-2 mb-1"></i>Change Color
+          <i class="cil-contrast mr-2 mb-1"></i>{{ selectedText }}
         </template>
-        <b-dropdown-item-button v-for="(option, index) in colorOptions" :key="index" @click="changeColor(option)">
+        <b-dropdown-item-button
+          v-for="(option, index) in colorOptions"
+          :key="index"
+          @click="changeColor(option)"
+        >
           {{ option.text }}
         </b-dropdown-item-button>
       </b-dropdown>
-      <b-form-checkbox class="float-right mr-3 mt-2" id="simplified" v-model="simplified" :disabled="isBusy">Simplifiied
-        View</b-form-checkbox>
+      <b-form-checkbox
+        class="float-right mr-3 mt-2"
+        id="simplified"
+        v-model="simplified"
+        :disabled="isBusy"
+        >Simplifiied View</b-form-checkbox
+      >
     </div>
     <div class="card-body">
       <div class="card-text">
-        <highcharts class="hc" v-if="!isBusy" :options="chartOptions" ref="chart"></highcharts>
+        <highcharts
+          class="hc"
+          :options="chartOptions"
+          :updateArgs="[true, true]"
+          ref="chart"
+        ></highcharts>
       </div>
     </div>
   </div>
@@ -34,7 +57,7 @@ Networkgraph(Highcharts)
 
 export default {
   name: 'AppGraph',
-  computed: mapState(['filterId', 'colorOptions','colors']),
+  computed: mapState(['filterId', 'colorOptions', 'colors']),
   mixins: [msgMixin],
   data() {
     return {
@@ -42,6 +65,7 @@ export default {
       isBusy: false,
       simplified: true,
       selectedColor: '',
+      selectedText: '',
       colorScale: null,
       chartOptions: {
         credits: {
@@ -54,32 +78,37 @@ export default {
         title: {
           text: ''
         },
-        series: [{
-          enableMouseTracking: true,
-          layoutAlgorithm: {
-            enableSimulation: true,
-            initialPositions: 'circle'
-          },
-          dataLabels: {
-            enabled: false
-          },
-          name: 'networkgraph',
-          data: [],
-          nodes: []
-        }]
+        series: [
+          {
+            enableMouseTracking: true,
+            layoutAlgorithm: {
+              enableSimulation: true,
+              initialPositions: 'circle'
+            },
+            dataLabels: {
+              enabled: false
+            },
+            name: 'networkgraph',
+            data: [],
+            nodes: []
+          }
+        ]
       }
     }
   },
   methods: {
     changeColor(option) {
       this.selectedColor = option.value
-      this.infoMsg(`Coloring graph by ${option.text}`)
-      this.getGraph()
+      this.selectedText = option.text
+      this.getGraph(true)
     },
-    getGraph() {
+    getGraph(colorOnly = false) {
       this.isBusy = true
-      this.chartOptions.series[0].data = []
+      // trying to find a better way to update the nodes, work in progress
       this.chartOptions.series[0].nodes = []
+      if (!colorOnly) {
+        this.chartOptions.series[0].data = []
+      }
       const url = '/api/graph'
       const body = {
         color_name: this.selectedColor,
@@ -94,11 +123,13 @@ export default {
         .then((res) => {
           let nodes = res.data.nodes
           // // select color for each node
-          nodes.forEach(node => {
+          nodes.forEach((node) => {
             node.color = this.colorScale(node.colorScale).toString()
           })
           this.chartOptions.series[0].nodes = nodes
-          this.chartOptions.series[0].data = res.data.data
+          if(!colorOnly) {
+            this.chartOptions.series[0].data = res.data.data
+          }
         })
         .catch((err) => {
           console.error(err)
@@ -120,6 +151,7 @@ export default {
   created() {
     this.colorScale = chroma.scale([this.colors.secondary, this.colors.primary])
     this.selectedColor = this.colorOptions[0].value
+    this.selectedText = this.colorOptions[0].text
     this.getGraph()
   }
 }
