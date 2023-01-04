78d15c0c7697f131c477ae32cec8b3818d0518e7
mechevar <michael.echevarria@symphonyai.com>
2023-01-04 16:14:27 -0500

    simple page for showing the ID

---
+++ frontend-vue/src/components/PatientDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-deck mb-3">
+    <div class="card">
+      <div class="card-header">
+        <i class="spinner-border spinner-border-sm mr-1" v-if="isBusy"></i>
+        Detail
+      </div>
+      <div class="card-body">
+        <p class="card-text"><pre>Selected ID {{ id }}</pre></p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import msgMixin from '../mixins/msg-mixin'
+
+export default {
+  name: 'AppPatientDetail',
+  mixins: [msgMixin],
+  data() {
+    return {
+      id: 0
+    }
+  },
+  
+  created() {
+      this.id = this.$route.params.id
+  }
+}
+</script>
