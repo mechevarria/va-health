9053d94826b9b370eaf9dc35312fa46f56873edd
mechevar <michael.echevarria@symphonyai.com>
2023-01-13 14:53:31 -0500

    coniditonal coloring for scores, re-align all card

--- frontend-vue/src/components/PatientDetail.vue
+++ frontend-vue/src/components/PatientDetail.vue
@@ -1,114 +1,136 @@
 <template>
-  <span>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="card mb-4">
-            <div class="card-body text-center">
+  <div class="container">
+    <div class="card-deck mb-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-3">
               <img
                 src="../assets/patient-icon.png"
                 alt="avatar"
                 class="img-fluid"
                 style="width: 150px"
               />
-              <h5 class="my-3">Patient Physical</h5>
-              <p class="mb-1">
-                ID <span class="text-muted">{{ id }}</span>
-              </p>
-              <i class="spinner-border spinner-border-sm ml-1" v-if="isBusy" ></i>
-              <p
-                class="mb-1"
-                v-for="([key, value], index) in Object.entries(data.physical)"
-                :key="index"
-              >
-                {{ key }} <span class="text-muted">{{ value }}</span>
-              </p>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-8">
-          <div class="card mb-4">
-            <div class="card-body">
-              <p class="card-text">
-                <strong class="mb-2">Demographics</strong>
+            <div class="col-sm-9 align-self-center">
+              <h5 class="my-3">
+                Patient Physical
                 <i
                   class="spinner-border spinner-border-sm ml-1"
                   v-if="isBusy"
                 ></i>
-              </p>
-              <span
-                v-for="([key, value], index) in Object.entries(
-                  data.demographics
-                )"
-                :key="index"
-              >
-                <div class="row">
-                  <div class="col-sm-3">
-                    <p class="mb-0 ml-2">{{ key }}</p>
-                  </div>
-                  <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ value }}</p>
-                  </div>
-                </div>
-                <hr class="ml-2" />
-              </span>
+              </h5>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="card mb-4 mb-md-0">
-                <div class="card-body">
-                  <p class="card-text">
-                    <strong class="mb-2">Risk Scores</strong>
-                    <i
-                      class="spinner-border spinner-border-sm ml-1"
-                      v-if="isBusy"
-                    ></i>
-                  </p>
-                  <div class="row">
-                    <div class="col-md-5">
-                      <div class="c-callout c-callout-danger">
-                        <small class="text-muted">A1C Increase Risk</small><br />
-                        <strong class="h4">{{ data.risk_scores.a1c_increase_risk }}</strong>
-                      </div>
-                    </div>
-                    <!--/.col-->
-                    <div class="col-md-7">
-                      <div class="c-callout c-callout-info">
-                        <small class="text-muted">Engagement Decrease Risk</small><br />
-                        <strong class="h4">{{ data.risk_scores.engagement_decrease_risk }}</strong>
-                      </div>
-                    </div>
-                    <!--/.col-->
-                  </div>
-                </div>
-              </div>
+
+          <div class="row ml-3">
+            <div class="col-sm-5">
+              <p class="mb-0">ID</p>
             </div>
-            <div class="col-md-6">
-              <div class="card mb-4 mb-md-0">
-                <div class="card-body">
-                  <p class="card-text">
-                    <strong class="mb-2">Comorbidities</strong>
-                    <i
-                      class="spinner-border spinner-border-sm ml-1"
-                      v-if="isBusy"
-                    ></i>
-                    <br />
-                    <span
-                      class="badge rounded-pill bg-danger text-white m-2"
-                      v-for="(key, index) in Object.keys(data.comorbidities)"
-                      :key="index"
-                      >{{ key }}</span
-                    >
-                  </p>
-                </div>
-              </div>
+            <div class="col-sm-7">
+              <p class="text-muted mb-0">{{ id }}</p>
+            </div>
+          </div>
+          <div
+            class="row ml-3"
+            v-for="([key, value], index) in Object.entries(data.physical)"
+            :key="index"
+          >
+            <div class="col-sm-5">
+              <p class="mb-0">{{ key }}</p>
+            </div>
+            <div class="col-sm-7">
+              <p class="text-muted mb-0">
+                <span
+                  v-if="value === 'Yes'"
+                  class="badge rounded-pill bg-danger text-white"
+                >
+                  {{ value }}
+                </span>
+                <span v-else>
+                  {{ value }}
+                </span>
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+            <strong class="mb-2">Demographics</strong>
+            <i class="spinner-border spinner-border-sm ml-1" v-if="isBusy"></i>
+          </p>
+          <span
+            v-for="([key, value], index) in Object.entries(data.demographics)"
+            :key="index"
+          >
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0 ml-2">{{ key }}</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ value }}</p>
+              </div>
+            </div>
+            <hr class="ml-2" />
+          </span>
+        </div>
+      </div>
     </div>
-  </span>
+    <div class="card-deck">
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+            <strong class="mb-2">Risk Scores</strong>
+            <i class="spinner-border spinner-border-sm ml-1" v-if="isBusy"></i>
+          </p>
+          <div class="row">
+            <div class="col-md-5">
+              <div
+                class="c-callout"
+                :class="getClassName(data.risk_scores.a1c_increase_risk)"
+              >
+                <small class="text-muted">A1C Increase Risk</small><br />
+                <strong class="h4">{{
+                  data.risk_scores.a1c_increase_risk
+                }}</strong>
+              </div>
+            </div>
+            <!--/.col-->
+            <div class="col-md-7">
+              <div
+                class="c-callout"
+                :class="getClassName(data.risk_scores.engagement_decrease_risk)"
+              >
+                <small class="text-muted">Engagement Decrease Risk</small><br />
+                <strong class="h4">{{
+                  data.risk_scores.engagement_decrease_risk
+                }}</strong>
+              </div>
+            </div>
+            <!--/.col-->
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <p class="card-text">
+            <strong class="mb-2">Comorbidities</strong>
+            <i class="spinner-border spinner-border-sm ml-1" v-if="isBusy"></i>
+            <br />
+            <span
+              class="badge rounded-pill bg-danger text-white m-2"
+              v-for="(key, index) in Object.keys(data.comorbidities)"
+              :key="index"
+              >{{ key }}</span
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import axios from 'axios'
@@ -131,6 +153,18 @@ export default {
     }
   },
   methods: {
+    getClassName(score) {
+      switch (true) {
+        case score < 0.333:
+          return 'c-callout-success'
+        case score > 0.333 && score < 0.666:
+          return 'c-callout-warning'
+        case score > 0.666:
+          return 'c-callout-danger'
+        default:
+          return 'c-callout-info'
+      }
+    },
     getDetails() {
       this.isBusy = true
       const url = `/api/patient/${this.id}`
