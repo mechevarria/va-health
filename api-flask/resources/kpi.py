8846cc98109640d5373c1303255b06182e5d28bf
Eric Murray <eric.murray@crescendoai.com>
2023-01-17 17:09:08 -0600

    updated kpi endpoint to work with final column names

--- api-flask/resources/kpi.py
+++ api-flask/resources/kpi.py
@@ -20,21 +20,21 @@ def compute_kpis(src, group_id):
   kpis.append({"name": "Total Patients", "value": grp['row_count']})
 
   #Get number of vaccinated
-  vac_values = group_stat(src, grp, 'TotalSeriesCount')
+  vac_values = group_stat(src, grp, 'covid_vaccine_TotalSeriesCount')
   vac_percent = sum([i>0 for i in vac_values]) / len(vac_values) * 100
   kpis.append({"name": "Vaccination Precentage", "value": f"{vac_percent:.1f}%"})
   
   #Get Hospitilization rate
-  hr = group_stat(src, grp, 'Hospitalization60d')
+  hr = group_stat(src, grp, 'covid_post_procedures_Hospitalization60d')
   hrp = sum(hr) / len(hr) * 100 
   kpis.append({"name": "Hospitilization Rate", "value":  f"{hrp:.1f}%"})
 
-  in_per_vis = group_stat(src, grp, 'visits_count_proportion_Presumed In Person_period4_2021-03-01_2022-03-01')
+  in_per_vis = group_stat(src, grp, 'visits_count_proportion_Presumed In Person_period3')
   in_per_vis = sum(in_per_vis) / len(in_per_vis) * 100
   kpis.append({"name": "In Person Visit %", "value": f"{in_per_vis:.1f}%"})
 
   #Get average a1c for the group
-  a1c = group_stat(src, grp, 'A1C_last_period4_2021-03-01_2022-03-01')
+  a1c = group_stat(src, grp, 'A1C_last_period4')
   a1c = sum(a1c) / len(a1c) 
   kpis.append({"name": "Average A1C", "value": f"{a1c:.2f}"})
 
