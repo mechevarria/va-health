88639d5131053b46c1ffd8340f74e4da980bc941
mechevar <michael.echevarria@symphonyai.com>
2023-01-31 16:15:41 -0500

    add color key for values

--- frontend-vue/src/components/Graph.vue
+++ frontend-vue/src/components/Graph.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <span
-        >{{ `Graph ${label}` }}
-        <i class="spinner-border spinner-border-sm mr-1 ml-1" v-if="isBusy"></i
-      ></span>
-
-      <b-dropdown
-        class="float-right"
-        id="color-dropdown"
-        right
-        text="Right align"
-        :disabled="isBusy"
-      >
-        <template #button-content>
-          <i class="cil-contrast mr-2 mb-1"></i>{{ selectedText }}
-        </template>
-        <b-dropdown-item-button
-          v-for="(option, index) in colorOptions"
-          :key="index"
-          @click="changeColor(option)"
-        >
-          {{ option.text }}
-        </b-dropdown-item-button>
-      </b-dropdown>
-      <b-form-checkbox
-        class="float-right mr-3 mt-2"
-        v-model="simplified"
-        :id="`simplified-${label}`"
-        :disabled="isBusy"
-        >Simplified</b-form-checkbox
-      >
+      <div class="d-flex">
+        <div class="mr-auto mt-1">
+          {{ `Graph ${label}` }}
+          <i
+            class="spinner-border spinner-border-sm mr-1 ml-1"
+            v-if="isBusy"
+          ></i>
+        </div>
+        <div class="mr-3">
+          <highcharts
+            class="hc"
+            :options="colorChart"
+            v-if="!isBusy"
+          ></highcharts>
+        </div>
+        <div class="mr-3">
+          <b-dropdown
+            class="float-right"
+            id="color-dropdown"
+            right
+            text="Right align"
+            :disabled="isBusy"
+          >
+            <template #button-content>
+              <i class="cil-contrast mr-2 mb-1"></i>{{ selectedText }}
+            </template>
+            <b-dropdown-item-button
+              v-for="(option, index) in colorOptions"
+              :key="index"
+              @click="changeColor(option)"
+            >
+              {{ option.text }}
+            </b-dropdown-item-button>
+          </b-dropdown>
+        </div>
+        <div class="mt-2">
+          <b-form-checkbox
+            v-model="simplified"
+            :id="`simplified-${label}`"
+            :disabled="isBusy"
+            >Simplified</b-form-checkbox
+          >
+        </div>
+      </div>
     </div>
     <div class="card-body">
       <div class="card-text" ref="chartContainer">
         <highcharts
           class="hc"
-          :options="chartOptions"
+          :options="graphOptions"
           v-if="showChart"
         ></highcharts>
       </div>
@@ -74,7 +88,7 @@ export default {
       selectedText: '',
       colorScale: null,
       chartWidth: 0,
-      chartOptions: {
+      graphOptions: {
         credits: {
           enabled: false
         },
@@ -99,16 +113,128 @@ export default {
             point: {
               events: {
                 click: (e) => {
-                  if(e.point.groupId > 0) {
+                  if (e.point.groupId > 0) {
                     this.$store.commit('setGroup', e.point.groupId)
                   } else {
-                    console.warn(`node: { id: ${e.point.id} }, { groupId: ${e.point.groupId}}`)
+                    console.warn(
+                      `node: { id: ${e.point.id} }, { groupId: ${e.point.groupId}}`
+                    )
                   }
                 }
               }
             },
             data: [],
             nodes: []
+          }
+        ]
+      },
+      colorChart: {
+        exporting: {
+          enabled: false
+        },
+        chart: {
+          type: 'bar',
+          height: '30',
+          width: '200',
+          backgroundColor: null,
+          spacingTop: 0,
+          spacingRight: 0,
+          spacingBottom: 0,
+          spacingLeft: 0,
+          plotBorderWidth: 0,
+          margin: [10, 0, 0, 0]
+        },
+        credits: {
+          enabled: false
+        },
+        title: {
+          text: ''
+        },
+        yAxis: {
+          visible: false,
+          minPadding: 0,
+          maxPadding: 0
+        },
+        xAxis: {
+          visible: false,
+          minPadding: 0,
+          maxPadding: 0
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          bar: {
+            borderWidth: 0
+          },
+          series: {
+            enableMouseTracking: false,
+            stacking: 'normal'
+          }
+        },
+        series: [
+          {
+            data: [
+              {
+                y: 5,
+                name: '',
+                dataLabels: {
+                  enabled: true,
+                  y: -10,
+                  x: 5,
+                  align: 'right',
+                  format: '{point.name}'
+                },
+                color: {
+                  linearGradient: {
+                    x1: 1,
+                    x2: 1,
+                    y1: 1,
+                    y2: 0
+                  },
+                  stops: []
+                }
+              }
+            ]
+          },
+          {
+            data: [
+              {
+                y: 0,
+                name: '',
+                color: '',
+                dataLabels: {
+                  enabled: true,
+                  y: -10,
+                  align: 'center',
+                  format: '{point.name}'
+                }
+              }
+            ]
+          },
+          {
+            data: [
+              {
+                y: 5,
+                name: '',
+                dataLabels: {
+                  enabled: true,
+                  y: -10,
+                  x: -5,
+                  align: 'left',
+                  format: '{point.name}'
+                },
+                color: {
+                  linearGradient: {
+                    x1: 1,
+                    x2: 1,
+                    y1: 1,
+                    y2: 0
+                  },
+                  stops: []
+                }
+              }
+            ]
           }
         ]
       }
@@ -122,8 +248,8 @@ export default {
     },
     getGraph() {
       this.isBusy = true
-      this.chartOptions.series[0].nodes = []
-      this.chartOptions.series[0].data = []
+      this.graphOptions.series[0].nodes = []
+      this.graphOptions.series[0].data = []
 
       const url = '/api/graph'
       const body = {
@@ -138,12 +264,17 @@ export default {
       axios
         .post(url, body)
         .then((res) => {
+          // set labels for color scale
+          this.colorChart.series[0].data[0].name = res.data.color_controls.color_high
+          this.colorChart.series[1].data[0].name = res.data.color_controls.color_middle
+          this.colorChart.series[2].data[0].name = res.data.color_controls.color_low
+
           let nodes = res.data.nodes
           nodes.forEach((node) => {
             node.color = this.colorScale(node.colorScale).toString()
           })
-          this.chartOptions.series[0].nodes = nodes
-          this.chartOptions.series[0].data = res.data.data
+          this.graphOptions.series[0].nodes = nodes
+          this.graphOptions.series[0].data = res.data.data
         })
         .catch((err) => {
           console.error(err)
@@ -162,23 +293,39 @@ export default {
       this.getGraph()
     },
     doRedraw() {
-      if(this.doRedraw && this.label === 1) {
+      if (this.doRedraw && this.label === 1) {
         this.showChart = false
         setTimeout(() => {
           this.showChart = true
           this.$store.commit('noRedraw')
-        },10)
+        }, 10)
       }
     }
   },
   mounted() {
-    if(this.label > 1) {
+    if (this.label > 1) {
       this.simplified = false
     }
     const red = chroma(this.colors.danger).darken(0.6)
-    const yellow = this.colors.warning
+    const yellow = chroma(this.colors.warning)
     const green = chroma(this.colors.success)
+
     this.colorScale = chroma.scale([red, yellow, green])
+
+    // mid to high
+    this.colorChart.series[0].data[0].color.stops = [
+      [0, String(yellow)],
+      [1, String(green)]
+    ]
+    // mid
+    this.colorChart.series[1].data[0].color = String(yellow)
+
+    // low to mid
+    this.colorChart.series[2].data[0].color.stops = [
+      [0, String(red)],
+      [1, String(yellow)]
+    ]
+
     this.selectedColor = this.colorOptions[this.label - 1].value
     this.selectedText = this.colorOptions[this.label - 1].text
     this.getGraph()
